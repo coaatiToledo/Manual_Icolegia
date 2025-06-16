b90014dfb91d9147c932ed3c9ac6a1c7e6e4fe41
OmarOrtez <alumno1085304@is-azarquiel.es>
2025-06-16 13:32:34 +0200

    centrando los h1

--- manual-icolegia/src/components/Documentacion.jsx
+++ manual-icolegia/src/components/Documentacion.jsx
@@ -12,7 +12,7 @@ function Documentacion() {
   };
   return (
     <div className="  px-4 py-8 flex flex-col gap-8">
-      <h1 className="text-5xl font-semibold text-blue-700 mb-8 ml-45 ">Generar y Enviar Documentación</h1>
+      <h1 className="text-5xl font-semibold text-blue-700 mb-8 ml-45 text-center">Generar y Enviar Documentación</h1>
         <ZonaGenerarDoc />
         <GenerarDocumentacion />
         <AdjuntarDoc />
